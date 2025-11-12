038bbd69580a882c758958b1c70314145127f849
EstudianteUC <estudianteUC@UCUXB100220.continental.edu.pe>
2025-11-12 11:37:16 -0500

    Eliminar archivo innecesario Untitled-1.py

--- Untitled-1.py
+++
@@ -1,6 +0,0 @@
-def saludo(nombre: str) -> str:
-    return f"Hola, {nombre}! Bienvenido a Git, commit exitoso."
-
-if __name__ == "__main__":
-    nombre = input("¿Tu nombre? ")
-    print(saludo(nombre)) 
